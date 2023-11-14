6ebf3594f34574ec6500067b01be0f08f8e082d6
wachinlai01 <manumalaguista99@gmail.com>
2023-11-14 18:28:42 +0100

    Añadidio boton a crear cuenta

--- inicio_sesion.php
+++ inicio_sesion.php
@@ -47,6 +47,9 @@
             </div>
             <input class="btn btn-primary" type="submit" value="Iniciar sesion">
         </form>
+        <button class="btn btn-dark" style= "float:auto; margin:10px">
+                    <a href="registroUsuario.php" style="text-decoration:none; color:white">Regístrate</a>
+        </button>
     </div>
     </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
